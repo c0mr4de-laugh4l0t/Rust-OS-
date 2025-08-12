9215b0599d8c5c76afb68cb57adfdd82581a5c3b
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-08-12 17:30:43 +0300

    Update main.rs

--- IronVeil/src/main.rs
+++ IronVeil/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Write};
+use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
+use std::path::Path;
 
 fn main() {
     banner();
@@ -16,10 +18,48 @@ fn main() {
                 println!("\x1b[38;5;208mAvailable commands:\x1b[0m");
                 println!("  help    - Show this help message");
                 println!("  clear   - Clear the screen");
+                println!("  boot    - Build & boot the Nexis kernel in QEMU");
                 println!("  exit    - Quit IronVeil CLI");
             }
             "clear" => {
                 print!("\x1b[2J\x1b[H");
+            }
+            "boot" => {
+                let kernel_path = "../Nexis/target/x86_64-nexis/debug/bootimage-nexis.bin";
+
+                if !Path::new(kernel_path).exists() {
+                    println!("\x1b[38;5;208mBoot image not found. Building Nexis kernel...\x1b[0m");
+                    let status = Command::new("cargo")
+                        .args(&["bootimage"])
+                        .current_dir("../Nexis")
+                        .status();
+
+                    match status {
+                        Ok(s) if s.success() => println!("\x1b[38;5;208mBuild successful!\x1b[0m"),
+                        Ok(s) => {
+                            eprintln!("\x1b[31mBuild failed with status {:?}\x1b[0m", s.code());
+                            continue;
+                        }
+                        Err(e) => {
+                            eprintln!("\x1b[31mFailed to run cargo bootimage: {}\x1b[0m", e);
+                            continue;
+                        }
+                    }
+                }
+
+                println!("\x1b[38;5;208mBooting Nexis kernel in QEMU...\x1b[0m");
+                let status = Command::new("qemu-system-x86_64")
+                    .args(&[
+                        "-drive", &format!("format=raw,file={}", kernel_path),
+                        "-serial", "stdio",
+                        "-m", "512M",
+                    ])
+                    .status();
+
+                match status {
+                    Ok(s) => println!("\x1b[38;5;208mQEMU exited with status: {:?}\x1b[0m", s.code()),
+                    Err(e) => eprintln!("\x1b[31mFailed to run QEMU: {}\x1b[0m", e),
+                }
             }
             "exit" => {
                 println!("\x1b[38;5;208mExiting IronVeil CLI...\x1b[0m");
