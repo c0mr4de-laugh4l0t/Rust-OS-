67d92b713af72d795ab237510871537f2f91f138
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-08-12 17:15:12 +0300

    Update syscall.rs

--- Nexis/src/syscall.rs
+++ Nexis/src/syscall.rs
@@ -1,7 +1,14 @@
+#![no_std]
+
 pub fn call(num: u64, a1: usize, a2: usize, a3: usize) -> usize {
     match num {
         0 => sys_write(a1 as *const u8, a2),
         1 => sys_exit(a1 as i32),
+        2 => crate::fs::fs_open(a1 as *const u8, a2),
+        3 => crate::fs::fs_read(a1, a2 as *mut u8, a3),
+        4 => crate::fs::fs_write(a1, a2 as *const u8, a3),
+        5 => crate::fs::fs_close(a1),
+        6 => crate::fs::fs_create(a1 as *const u8, a2, a3 as *const u8, 0),
         _ => usize::MAX,
     }
 }
