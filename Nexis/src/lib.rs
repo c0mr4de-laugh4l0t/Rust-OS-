70190eb74d39000b65a9c5f0ff1e5f56b378763e
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-08-13 17:54:11 +0300

    Update lib.rs

--- Nexis/src/lib.rs
+++ Nexis/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
 pub mod interrupts;
 pub mod vga;
 pub mod kb;
@@ -8,6 +9,7 @@ pub mod memory;
 pub mod scheduler;
 pub mod task;
 pub mod fs;
+mod alloc;
 
 // re-export PMM if needed
 pub use memory::PhysicalMemoryManager;
