c32df1866fc91ee409a722df916b6cb84dbe3765
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-08-29 20:00:19 +0300

    Update build.rs

--- Nexis/build.rs
+++ Nexis/build.rs
@@ -1,9 +1,19 @@
-fn main() {
-    // linker script arg (if you're using one)
-    println!("cargo:rustc-link-arg=-Tlinker.ld");
+use std::env;
+use std::path::PathBuf;
 
-    // assemble context.S
+fn main() {
+    // Compile assembly (switch.S)
+    println!("cargo:rerun-if-changed=src/asm/switch.S");
     cc::Build::new()
-        .file("src/context.S")
-        .compile("context_switch");
+        .file("src/asm/switch.S")
+        .flag_if_supported("-march=x86-64")
+        .compile("switch");
+
+    // Ensure linker script is passed to the linker
+    println!("cargo:rerun-if-changed=linker.ld");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let linker = PathBuf::from(manifest_dir).join("linker.ld");
+    println!("cargo:rustc-link-arg=-T{}", linker.display());
+
+    // Optional: allow embedding other artifacts later
 }
