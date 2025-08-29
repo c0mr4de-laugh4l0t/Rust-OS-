a3aa87976a771239d066df5b7556f13e9ad76186
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-08-29 19:29:44 +0300

    Update lib.rs

--- Nexis/src/lib.rs
+++ Nexis/src/lib.rs
@@ -1,15 +1,19 @@
 #![no_std]
-#![no_main]
 
-extern crate alloc;
+pub mod alloc;
+pub mod context;
 pub mod interrupts;
-pub mod vga;
+pub mod pit;
 pub mod kb;
+pub mod vga;
 pub mod memory;
-pub mod scheduler;
 pub mod task;
+pub mod scheduler;
+pub mod process;
+pub mod syscall;
+pub mod syscall_dispatch;
 pub mod fs;
-mod alloc;
+pub mod userland;
 
 #[alloc_error_handler]
 fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
