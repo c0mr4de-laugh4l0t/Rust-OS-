f3b010001b3adf4258ea73e0987703fde12f897e
c0mr4de-laugh4l0t <maidhajama01@gmail.com>
2025-09-03 14:52:20 +0300

    Update vga.rs

--- Nexis/src/vga.rs
+++ Nexis/src/vga.rs
@@ -2,7 +2,6 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use uart_16550::SerialPort;
 use core::fmt::Write;
-use x86_64::instructions::port::Port;
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
@@ -17,12 +16,18 @@ lazy_static! {
     };
 }
 
+// ---- Serial printing ----
 pub fn serial_print(args: core::fmt::Arguments) {
     let mut s = SERIAL1.lock();
     let _ = s.write_fmt(args);
 }
+pub fn serial_println(args: core::fmt::Arguments) {
+    serial_print(args);
+    serial_print(format_args!("\n"));
+}
+
 macro_rules! sprintln {
-    ($($arg:tt)*) => (crate::vga::serial_print(format_args!($($arg)*)));
+    ($($arg:tt)*) => (crate::vga::serial_println(format_args!($($arg)*)));
 }
 macro_rules! sprint {
     ($($arg:tt)*) => (crate::vga::serial_print(format_args!($($arg)*)));
@@ -30,12 +35,19 @@ macro_rules! sprint {
 pub(crate) use sprintln;
 pub(crate) use sprint;
 
+// ---- VGA Writer ----
 pub struct VgaWriter {
     column: usize,
     row: usize,
     color: u8,
     buffer: *mut u8,
 }
+
+// Safety: VgaWriter writes directly to a memory-mapped buffer.
+// We guarantee only one global instance, so it's safe.
+unsafe impl Send for VgaWriter {}
+unsafe impl Sync for VgaWriter {}
+
 impl VgaWriter {
     pub const fn new() -> Self {
         Self {
@@ -45,6 +57,7 @@ impl VgaWriter {
             buffer: VGA_BUFFER_ADDR as *mut u8,
         }
     }
+
     pub fn put_char(&mut self, c: char) {
         match c {
             '\n' => { self.new_line(); return; }
@@ -59,23 +72,26 @@ impl VgaWriter {
         }
         self.column += 1;
     }
+
     pub fn write_str(&mut self, s: &str) {
         for c in s.chars() { self.put_char(c); }
     }
+
     pub fn new_line(&mut self) {
         self.column = 0;
-        if self.row + 1 < BUFFER_HEIGHT { self.row += 1; }
-        else {
+        if self.row + 1 < BUFFER_HEIGHT {
+            self.row += 1;
+        } else {
             // scroll up
             for r in 1..BUFFER_HEIGHT {
                 for col in 0..BUFFER_WIDTH {
                     let src = ((r * BUFFER_WIDTH) + col) * 2;
-                    let dst = (((r - 1)*BUFFER_WIDTH)+col)*2;
+                    let dst = (((r - 1) * BUFFER_WIDTH) + col) * 2;
                     unsafe {
                         let ch = core::ptr::read_volatile(self.buffer.add(src));
-                        let color = core::ptr::read_volatile(self.buffer.add(src+1));
+                        let color = core::ptr::read_volatile(self.buffer.add(src + 1));
                         core::ptr::write_volatile(self.buffer.add(dst), ch);
-                        core::ptr::write_volatile(self.buffer.add(dst+1), color);
+                        core::ptr::write_volatile(self.buffer.add(dst + 1), color);
                     }
                 }
             }
@@ -83,12 +99,13 @@ impl VgaWriter {
             let last = (BUFFER_HEIGHT - 1) * BUFFER_WIDTH * 2;
             for col in 0..BUFFER_WIDTH {
                 unsafe {
-                    core::ptr::write_volatile(self.buffer.add(last + col*2), b' ');
-                    core::ptr::write_volatile(self.buffer.add(last + col*2 + 1), self.color);
+                    core::ptr::write_volatile(self.buffer.add(last + col * 2), b' ');
+                    core::ptr::write_volatile(self.buffer.add(last + col * 2 + 1), self.color);
                 }
             }
         }
     }
+
     pub fn clear_screen(&mut self) {
         for r in 0..BUFFER_HEIGHT {
             for c in 0..BUFFER_WIDTH {
@@ -99,7 +116,8 @@ impl VgaWriter {
                 }
             }
         }
-        self.row = 0; self.column = 0;
+        self.row = 0;
+        self.column = 0;
     }
 }
 
@@ -110,16 +128,7 @@ impl core::fmt::Write for VgaWriter {
     }
 }
 
-pub fn serial_print(args: core::fmt::Arguments) {
-    let mut s = SERIAL1.lock();
-    let _ = s.write_fmt(args);
-}
-pub fn serial_println(args: core::fmt::Arguments) {
-    serial_print(args);
-    serial_print(format_args!("\n"));
-}
-
-// helpers to print to both
+// ---- VGA + Serial printing ----
 pub fn vprintln_impl(args: core::fmt::Arguments) {
     {
         let mut v = VGA_WRITER.lock();
@@ -129,6 +138,7 @@ pub fn vprintln_impl(args: core::fmt::Arguments) {
     serial_print(args);
     serial_print(format_args!("\n"));
 }
+
 macro_rules! vprintln {
     ($($arg:tt)*) => (crate::vga::vprintln_impl(format_args!($($arg)*)));
 }
